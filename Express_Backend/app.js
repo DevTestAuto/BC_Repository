f738f61f3dd89254b552cd129febdec54a9a9044
Your Name <you@example.com>
2019-03-07 11:44:58 +0530

    AWS RDS details replaced

--- Express_Backend/app.js
+++ Express_Backend/app.js
@@ -7,10 +7,10 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cors());
 const db = mysql.createConnection({
-    host: '192.168.10.26',
-    user: 'root',
-    password: 'root',
-    database: 'db_sp_eis'
+    host: 'bcemploymentinformationsystem.cnql3fsruc3y.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'admin123',
+    database: 'BCEmploymentInformationSystem'
 });
 
 db.connect();
@@ -90,7 +90,7 @@ app.get('/recordCount/:empUid', function (req, res) {
 });
 
 app.get('/cmpCount', function (req, res) {
-    var sql = 'SELECT count(*) as count FROM tbl_companies WHERE status = 1';
+    var sql = 'SELECT count(*) as count FROM tbl_companies';
     db.query(sql, (err, result) => {
         if (err) throw err;
         res.send(result);
@@ -226,7 +226,7 @@ app.post('/addCompReq', function (req, res) {
     });
 });
 
-// This method is used to handle company update request
+
 app.post('/editCompReq/:cid', function (req, res) {
     var data = {
         comp_name: req.body.comp_name, comp_type: req.body.comp_type,
@@ -246,10 +246,8 @@ app.post('/editCompReq/:cid', function (req, res) {
     });
 });
 
-// This method is used to handle employee update request
 app.post('/UpdateReq/:eid/:cid', function (req, res) {
     var data = {
-        
         emp_uid: req.body.emp_uid, emp_id: req.body.emp_id,
         emp_fname: req.body.emp_fname, emp_lname: req.body.emp_lname,
         emp_fathername: req.body.emp_fathername, emp_avatar: req.body.emp_avatar,
@@ -286,11 +284,47 @@ app.post('/UpdateReq/:eid/:cid', function (req, res) {
     });
 });
 
+// app.post('/InsertReq', function (req, res) {
+//     var data = {
+//         emp_uid: req.body.emp_uid, emp_id: req.body.emp_id,
+//         emp_fname: req.body.emp_fname, emp_lname: req.body.emp_lname,
+//         emp_fathername: req.body.emp_fathername, emp_avatar: req.body.emp_avatar,
+//         gender: req.body.gender, date_of_birth: req.body.date_of_birth,
+//         qualification: req.body.qualification, pan_number: req.body.pan_number,
+//         date_of_joining: req.body.date_of_joining, relieving_date: req.body.relieving_date,
+//         emp_type: req.body.emp_type, exit_type: req.body.exit_type,
+//         designation: req.body.designation, tech_expertise: req.body.tech_expertise,
+//         flag_verification: req.body.flag_verification, mgmt_status: req.body.mgmt_status,
+//         mgr_status: req.body.mgr_status, bc_status: req.body.bc_status,
+//         comp_id: req.body.comp_id, bc_count: req.body.bc_count,
+//         created_date: req.body.created_date, modified_date: req.body.modified_date
+//     };
+
+//     var sql = 'INSERT INTO tbl_employee SET ?';
+//     db.query(sql, data, (err, result) => {
+//         if (err) throw err;
+//         res.send({
+//             status: 'Data send successfully!',
+//             emp_uid: req.body.emp_uid, emp_id: req.body.emp_id,
+//             emp_fname: req.body.emp_fname, emp_lname: req.body.emp_lname,
+//             emp_fathername: req.body.emp_fathername, emp_avatar: req.body.emp_avatar,
+//             gender: req.body.gender, date_of_birth: req.body.date_of_birth,
+//             qualification: req.body.qualification, pan_number: req.body.pan_number,
+//             date_of_joining: req.body.date_of_joining, relieving_date: req.body.relieving_date,
+//             emp_type: req.body.emp_type, exit_type: req.body.exit_type,
+//             designation: req.body.designation, tech_expertise: req.body.tech_expertise,
+//             flag_verification: req.body.flag_verification, mgmt_status: req.body.mgmt_status,
+//             mgr_status: req.body.mgr_status, bc_status: req.body.bc_status,
+//             comp_id: req.body.comp_id, bc_count: req.body.bc_count,
+//             created_date: req.body.created_date, modified_date: req.body.modified_date
+//         });
+//     });
+// });
+
 app.listen(3210, () => {
     console.log('Server started successfully on port 3210')
 });
 
-// This method is used to handle date display
 function dateDisplayed(timestamp) {
     var date = new Date(timestamp);
     return (date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear());
